f987dc18cf612909a7fac58585a4f9acbe8e3b83
jaime <james18bt@gmail.com>
2021-07-05 22:58:29 -0400

    proyecto java pacientes 2

--- src/pacienteretro/Main.java
+++ src/pacienteretro/Main.java
@@ -13,36 +13,62 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
         
+        int rutPaciente = 11;
+        System.out.println("---------------------------------");
         pacienteRetrofit pac = new pacienteRetrofit();
-        System.out.println("Obtener pacientes");
+        System.out.println("Lista de todos los pacientes en la BD");
         
         List<Paciente> pacientes = pac.obtenerTodosLosPacientes();
         System.out.println(pacientes);
         
-        System.out.println("Obtener 1 paciente");
-        Paciente ps = pac.obtenerPaciente(11);
+        System.out.println("---------------------------------");
+        System.out.println("Obtener un paciente con rut " + rutPaciente);
+        Paciente ps = pac.obtenerPaciente(rutPaciente);
         System.out.println(ps);
         
+        System.out.println("---------------------------------");
+        Paciente paciente = new Paciente();
         try{
-        Paciente createPaciente = new Paciente();
-        createPaciente.setRut(11);
-        createPaciente.setNombre("Dross");
-        createPaciente.setDireccion("Americo vespucio norte 520");
-        createPaciente.setIsapre("Colmena");
-        System.out.println("Obtener  paciente creado");
-        Paciente crearPaciente = pac.crearPaciente(createPaciente);
+        
+        paciente.setRut(rutPaciente);
+        paciente.setNombre("Dross");
+        paciente.setDireccion("Americo vespucio norte 520");
+        paciente.setIsapre("Colmena");
+        System.out.println("Obtener  paciente creado con rut " + rutPaciente);
+        Paciente crearPaciente = pac.crearPaciente(paciente);
         System.out.println(crearPaciente);
         }catch(Exception e){
             System.out.println(e);
         }
+        System.out.println("---------------------------------");
+        System.out.println("Lista de todos los pacientes en la BD");
+        pacientes = pac.obtenerTodosLosPacientes();
+        System.out.println(pacientes);
+        System.out.println("---------------------------------");
+        System.out.println("Actualizar paciente con rut " + rutPaciente);
+        paciente.setRut(rutPaciente);
+        paciente.setNombre("Mario Castañeda");
+        paciente.setDireccion("Americo vespucio norte 23");
+        paciente.setIsapre("CONSALUD");
+        Paciente updatePaciente = pac.actualizarPaciente(paciente, rutPaciente);
+        System.out.println(updatePaciente);
         
+    System.out.println("---------------------------------");
+        System.out.println("Lista de todos los pacientes en la BD");
+        pacientes = pac.obtenerTodosLosPacientes();
+        System.out.println(pacientes);
         
-        
-       
-        System.out.println("Eliminar paciente"); 
-       ResponseBody rb = pac.borrarPaciente(11);
-       System.out.println(rb);
-        
+       System.out.println("---------------------------------");
+        System.out.println("Eliminar paciente con rut " + rutPaciente); 
+       ResponseBody rb = pac.borrarPaciente(rutPaciente);
+        System.out.println("---------------------------------");
+       System.out.println("Obtener un paciente con rut " + rutPaciente);
+        ps = pac.obtenerPaciente(rutPaciente);
+        System.out.println(ps);
+        System.out.println("---------------------------------");
+        System.out.println("Lista de todos los pacientes en la BD");
+        pacientes = pac.obtenerTodosLosPacientes();
+        System.out.println(pacientes);
        
     }
     
